c948dd030e09daf8db6acf51b39fe2f9600b759a
Joanna <joanna.gargas@gmail.com>
2018-04-15 00:38:28 +0200

    Add TestCase class and move self.name to it

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -1,7 +1,11 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
-        self.name = name
+        TestCase.__init__(self, name)
 
     def run(self):
         method = getattr(self, self.name)
